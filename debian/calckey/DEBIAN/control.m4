43f5a4cf48593eadd87207d980c9ef889941bbdb
Mizunashi Mana <contact@mizunashi.work>
2023-07-11 19:28:00 +0900

    Initial commit

---
+++ debian/calckey/DEBIAN/control.m4
@@ -0,0 +1,13 @@
+Package: calckey
+Version: __VERSION__
+Section: net
+Priority: optional
+Maintainer: Mizunashi Mana <contact@mizunashi.work>
+Architecture: __ARCH__
+Standards-Version: 4.6.2
+Homepage: https://github.com/mizunashi-mana/calckey-pkg
+Depends:    adduser,
+            nodejs (>= 18.0.0),
+            ffmpeg
+Rules-Requires-Root: no
+Description: Calckey service.
---
+++ debian/scripts/env.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CALCKEY_SOURCE_GIT_REPO="${CALCKEY_SOURCE_GIT_REPO:-"https://codeberg.org/calckey/calckey.git"}"
+
+CALCKEY_SOURCE_VERSION="${CALCKEY_SOURCE_VERSION:-v14.0.0-rc3}"
+PKG_VERSION_MAIN="$(echo "${CALCKEY_SOURCE_VERSION}" | sed -E 's/v([0-9.]+)-(.+)/\1+\2/')"
+PKG_VERSION="${PKG_VERSION_MAIN}-$(date +%Y%m%d%H%M%S)"
+
+WORK_DIR="${WORK_DIR:-"$(pwd)"}"
+
+SOURCE_DIR="${WORK_DIR}/source"
+INSTALL_DIR="${WORK_DIR}/install"

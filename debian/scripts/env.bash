d003f0be0f416231587b495ffad9dd0c4d3cf7df
Mizunashi Mana <contact@mizunashi.work>
2023-07-14 00:06:03 +0900

    Fix some

--- debian/scripts/env.bash
+++ debian/scripts/env.bash
@@ -6,7 +6,7 @@ CALCKEY_SOURCE_VERSION="${CALCKEY_SOURCE_VERSION:-v14.0.0-rc3}"
 PKG_VERSION_MAIN="$(echo "${CALCKEY_SOURCE_VERSION}" | sed -E 's/v([0-9.]+)-(.+)/\1+\2/')"
 PKG_VERSION="${PKG_VERSION_MAIN}-$(date +%Y%m%d%H%M%S)"
 
-WORK_DIR="${WORK_DIR:-"$(pwd)"}"
+WORK_DIR="${WORKDIR:-"$(cd "$(dirname "$0")/.." && pwd)"}"
 
 SOURCE_DIR="${WORK_DIR}/source"
 INSTALL_DIR="${WORK_DIR}/install"

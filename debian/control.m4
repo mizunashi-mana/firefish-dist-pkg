71c977c619044fbc1f45482c226fc8e6f8089687
Mizunashi Mana <contact@mizunashi.work>
2023-07-16 14:27:11 +0900

    Fix some settings

--- debian/control.m4
+++ debian/control.m4
@@ -7,7 +7,7 @@ Architecture: __ARCH__
 Standards-Version: 4.6.2
 Homepage: https://github.com/mizunashi-mana/calckey-pkg
 Depends:    adduser,
-            nodejs (>= 18.0.0),
+            nodejs (>= __NODE_MAJOR_VERSION__.0.0),
             ffmpeg
 Rules-Requires-Root: no
 Description: Calckey service.
--- debian/scripts/env.bash
+++ debian/scripts/env.bash
@@ -3,8 +3,10 @@
 CALCKEY_SOURCE_GIT_REPO="${CALCKEY_SOURCE_GIT_REPO:-"https://codeberg.org/calckey/calckey.git"}"
 
 CALCKEY_SOURCE_VERSION="${CALCKEY_SOURCE_VERSION:-v14.0.0-rc3}"
+NODE_MAJOR_VERSION="${NODE_MAJOR_VERSION:-20}"
+
 PKG_VERSION_MAIN="$(echo "${CALCKEY_SOURCE_VERSION}" | sed -E 's/v([0-9.]+)-(.+)/\1+\2/')"
-PKG_VERSION="${PKG_VERSION_MAIN}-$(date +%Y%m%d%H%M%S)"
+PKG_VERSION="${PKG_VERSION_MAIN}-$(date +%Y%m%d%H%M%S)-node${NODE_MAJOR_VERSION}"
 
 WORK_DIR="${WORKDIR:-"$(cd "$(dirname "$0")/.." && pwd)"}"
 

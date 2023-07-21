c707432715b2125d44c77058d6f063cfe9c45873
Mizunashi Mana <contact@mizunashi.work>
2023-07-22 04:51:51 +0900

    Fix repository url

--- debian/scripts/env.bash
+++ debian/scripts/env.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-FIREFISH_SOURCE_GIT_REPO="${FIREFISH_SOURCE_GIT_REPO:-"https://codeberg.org/firefish/firefish.git"}"
+FIREFISH_SOURCE_GIT_REPO="${FIREFISH_SOURCE_GIT_REPO:-"https://git.joinfirefish.org/firefish/firefish.git"}"
 
-FIREFISH_SOURCE_VERSION="${FIREFISH_SOURCE_VERSION:-"v1.0.1"}"
+FIREFISH_SOURCE_VERSION="${FIREFISH_SOURCE_VERSION:-"v1.0.3"}"
 NODE_MAJOR_VERSION="${NODE_MAJOR_VERSION:-20}"
 
 PKG_VERSION_MAIN="$(echo "${FIREFISH_SOURCE_VERSION}" | tr -d v | tr '-' '+')"

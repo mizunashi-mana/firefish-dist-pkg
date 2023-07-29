15de0be2a48ffafc769e7d6a811497c2d85a9902
Mizunashi Mana <contact@mizunashi.work>
2023-07-29 18:23:07 +0900

    Fix package and add test files

--- debian/scripts/env.bash
+++ debian/scripts/env.bash
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+DISTRO="${DISTRO:-bookworm}"
+
 FIREFISH_SOURCE_GIT_REPO="${FIREFISH_SOURCE_GIT_REPO:-"https://git.joinfirefish.org/firefish/firefish.git"}"
 
 FIREFISH_SOURCE_VERSION="${FIREFISH_SOURCE_VERSION:-"v1.0.3"}"
 NODE_MAJOR_VERSION="${NODE_MAJOR_VERSION:-20}"
 
 PKG_VERSION_MAIN="$(echo "${FIREFISH_SOURCE_VERSION}" | tr -d v | tr '-' '+')"
-PKG_VERSION="${PKG_VERSION_MAIN}-$(date +%Y%m%d%H%M%S)-node${NODE_MAJOR_VERSION}"
+PKG_DEB_VERSION="${PKG_DEB_VERSION:-"d$(date +%Y%m%d%H%M%S)-alpha"}"
+PKG_VERSION="${PKG_VERSION_MAIN}-${PKG_DEB_VERSION}"
 
 WORK_DIR="${WORKDIR:-"$(cd "$(dirname "$0")/.." && pwd)"}"
 
 SOURCE_DIR="${WORK_DIR}/source"
 INSTALL_DIR="${WORK_DIR}/install"
+DEST_DIR="${WORK_DIR}/dest"

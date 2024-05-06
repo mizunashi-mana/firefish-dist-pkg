3c7f7d512ded26515b6d93abb4bbe8fa9b74391a
Mizunashi Mana <contact@mizunashi.work>
2024-05-06 13:21:55 +0900

    Add deploy workflow of repo

--- repo/docker/entrypoint.bash
+++ repo/docker/entrypoint.bash
@@ -6,8 +6,8 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 
-GPG_KEY_FILE="${GPG_KEY_FILE:-"$PROJECT_DIR/docker/sample-gpg.key"}"
-GPG_PASS_FILE="${GPG_PASS_FILE:-"$PROJECT_DIR/docker/sample-gpg.pass"}"
+GPG_KEY_FILE="${GPG_KEY_FILE:-"$PROJECT_DIR/docker/gpg.key"}"
+GPG_PASS_FILE="${GPG_PASS_FILE:-"$PROJECT_DIR/docker/gpg.pass"}"
 cat "$GPG_KEY_FILE" | gpg --import --batch --yes
 
 "$PROJECT_DIR/scripts/build-repo" --passphrase-file "$GPG_PASS_FILE"
